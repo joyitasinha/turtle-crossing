b2816b92347bae42ade681f4f7a7a44b912ba0d3
joyitasinha <joyita.coding@gmail.com>
2022-09-08 23:10:23 +0300

    first commit

---
+++ player.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+UP = 90
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shapesize(stretch_wid=0.75, stretch_len=0.75)
+        self.setheading(UP)
+        self.shape("turtle")
+        self.reset()
+
+    def reset(self):
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.fd(MOVE_DISTANCE)
+
